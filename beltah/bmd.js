2c7936ce948929ff30489991335a7714d01ffdf5
joel James <165368500+jokathanjoka@users.noreply.github.com>
2024-07-12 21:45:25 +0300

    Update bmd.js

--- beltah/bmd.js
+++ beltah/bmd.js
@@ -12,7 +12,7 @@ zokou({
   'reaction': '🍾',
   'nomFichier': __filename
 }, async (_0x969b1f, _0x2501c9, _0x849359) => {
-  const _0x52fa52 = await fetch("https://api.github.com/repos/Beltahmd/beltah-md");
+  const _0x52fa52 = await fetch("https://github.com/jokathanjoka/BEST-CODER-MD/tree/main");
   const _0x34824f = await _0x52fa52.json();
   if (_0x34824f) {
     const _0x129946 = {
@@ -22,10 +22,10 @@ zokou({
       'owner': _0x34824f.owner.login
     };
     const _0x359c05 = new Date(_0x34824f.created_at).toLocaleDateString("en-GB");
-    const _0x321f6c = "  *❒⁠⁠⁠⁠BELTAH MULTI-DEVICE RANK❒⁠⁠⁠⁠* \n\n Beltahmd is a great WhatsApp bot created by the *Beltah* that has been given " + _0x129946.stars + " 🌟 stars as a sign of loving it and a total of " + _0x129946.forks " users have deployed it by now....🤩\n__________________________________\n ◔͜͡◔Made on Earth by ʙᴇʟᴛᴀʜ ᴋᴇɴʏᴀ◔͜͡◔";
+    const _0x321f6c = "  *❒BEST CODER MULTI-DEVICE RANK❒⁠⁠⁠⁠* \n\n best coder md is a great WhatsApp bot created by the *joel tech* that has been given " + _0x129946.stars + " 🌟 stars as a sign of loving it and a total of " + _0x129946.forks " users have deployed it by now....🤩\n__________________________________\n ◔͜͡◔Made on Earth by joel tech◔";
     await _0x2501c9.sendMessage(_0x969b1f, {
       'image': {
-        'url': "https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg"
+        'url': "https://telegra.ph/file/fe8a25fb17af3926e6048.jpg"
       },
       'caption': _0x321f6c
     });
