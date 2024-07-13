a1d181e6b4d3a9a9f00fbb2c38bdfeba398925d6
joel James <165368500+jokathanjoka@users.noreply.github.com>
2024-07-13 07:45:03 +0300

    Update and rename beltah/heroku.js to joelheroku.js

--- joelheroku.js
+++ joelheroku.js
@@ -11,7 +11,7 @@ zokou(
        const {ms,repondre,superUser , arg} = commandeOptions ;
        
        if(!superUser){repondre('only Mods can use this commande');return};
-       if(!arg[0] || !(arg.join('').split('='))) {repondre('Bad format ; Exemple of using :\nSetvar OWNER_NAME=Beltah');return};
+       if(!arg[0] || !(arg.join('').split('='))) {repondre('Bad format ; Exemple of using :\nSetvar OWNER_NAME=joel');return};
      
     const text = arg.join(" ")
      const Heroku = require("heroku-client");
@@ -48,7 +48,7 @@ zokou(
 			let baseURI = "/apps/" + s.HEROKU_APP_NAME;
 
             let h = await heroku.get(baseURI+'/config-vars')
-let str = '*Beltah-Md All Vars*\n\n'
+let str = '*best coder-Md All Vars*\n\n'
 for (vr in h) {
 str+= '◔͜͡◔ *'+vr+'* '+'= '+h[vr]+'\n'
 }
